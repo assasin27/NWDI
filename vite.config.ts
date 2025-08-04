7ebb88a55f2354b00c7ec1cdbb78289c20c5c8f5
mavenAakash <aakashsinghkhangar555@gmail.com>
2025-08-04 23:15:51 +0530

    vite.config alowed path and pacckge json removed t-er

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/T-ER/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   server: {
     host: "::",
     port: 8080,
@@ -25,9 +25,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
     allowedHosts: [
-      "groove-norfolk-initiatives-episode.trycloudflare.com",
       "t-er.onrender.com",
-      "amazing-unicorn-3f7416.netlify.app",
       "nwdi-f872.onrender.com", // Add your Render host here
     ],
   },
